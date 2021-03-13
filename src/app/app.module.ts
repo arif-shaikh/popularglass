5cd90a28629061e0e32d374daa8eb5d6bfe71c72
Arif Shaikh <arif.mohd.shaikh@gmail.com>
2021-03-13 16:55:11 +0530

    Initial app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { IvyCarouselModule } from './carousel/carousel.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IvyCarouselModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
