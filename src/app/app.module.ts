97aa772c2a5c7316aab6c238f269b075b7852fc0
Arif Shaikh <arif.mohd.shaikh@gmail.com>
2021-03-13 19:48:36 +0530

    Updated the design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 //import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { IvyCarouselModule } from './carousel/carousel.module';
+import {MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { IvyCarouselModule } from './carousel/carousel.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IvyCarouselModule
+    IvyCarouselModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
